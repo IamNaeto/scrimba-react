cd24a00f8e0e2113ebee735b78119e7180f74118
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2023-08-25 15:27:20 -0700

    props component mapping

--- jokes/src/App.jsx
+++ jokes/src/App.jsx
@@ -1,80 +1,11 @@
 import Jokes from './Jokes'
+import JokesData from './JokesData'
 
 function App() {
+  const jokesArr = JokesData.map((JokesElement) => <Jokes Setup={JokesElement.setup} Punchline={JokesElement.punchline}/>)
   return (
     <div className='jokes-container'>
-      <Jokes 
-        Punchline="It’s hard to explain puns to kleptomaniacs because 
-        they always take things literally."
-      />
-      <Jokes 
-        Setup="Why don't scientists trust atoms?"
-        Punchline="Because they make up everything!"
-      />
-      <Jokes 
-        Setup="Why don't skeletons fight each other?"
-        Punchline="They don't have the guts!"
-      />
-      <Jokes 
-        Setup="Parallel lines have so much in common."
-        Punchline="It's a shame they'll never meet."
-      />
-      <Jokes 
-        Setup="Why did the scarecrow win an award?"
-        Punchline="Because he was outstanding in his field!"
-      />
-      <Jokes 
-        Setup="Why did the bicycle fall over?"
-        Punchline="Because it was two-tired!"
-      />
-      <Jokes 
-        Setup="How do you organize a space party?"
-        Punchline="You 'planet'!"
-      />
-      <Jokes 
-        Setup="Why did the math book look sad?"
-        Punchline="Because it had too many problems."
-      />
-      <Jokes 
-        Setup="What's brown and sticky?"
-        Punchline="A stick!"
-      />
-      <Jokes 
-        Setup="How do you catch a squirrel?"
-        Punchline="Climb up in a tree and act like a nut!"
-      />
-      <Jokes 
-        Setup="Why don't oysters donate to charity?"
-        Punchline="Because they are shellfish!!"
-      />
-      <Jokes 
-        Setup="I used to play piano by ear, but now I use my hands"
-        Punchline="A carrot!"
-      />
-      <Jokes 
-        Setup="What's orange and sounds like a parrot?"
-        Punchline="It's more effective that way!"
-      />
-      <Jokes 
-        Setup="Why can't you hear a pterodactyl using the bathroom?"
-        Punchline="Because the 'P' is silent!"
-      />
-      <Jokes 
-        Setup="What do you call a can opener that doesn't work?"
-        Punchline=" A can't opener!"
-      />
-      <Jokes 
-        Setup="How do you organize a space party?"
-        Punchline="You 'planet'!"
-      />
-      <Jokes 
-        Setup="I got my daughter a fridge for her birthday."
-        Punchline="I can't wait to see her face light up when she opens it."
-      />
-      <Jokes 
-        Setup="How did the hacker escape the police?"
-        Punchline="He just ransomware!"
-      />
+      {jokesArr}
     </div>
   )
 }
---
+++ jokes/src/JokesData.jsx
@@ -0,0 +1,83 @@
+export default[
+    {
+      "setup": "Why don't scientists trust atoms?",
+      "punchline": "Because they make up everything!"
+    },
+    {
+      "setup": "Why don't skeletons fight each other?",
+      "punchline": "They don't have the guts!"
+    },
+    {
+      "setup": "Parallel lines have so much in common.",
+      "punchline": "It's a shame they'll never meet."
+    },
+    {
+      "setup": "Why did the scarecrow win an award?",
+      "punchline": "Because he was outstanding in his field!"
+    },
+    {
+      "setup": "Why did the bicycle fall over?",
+      "punchline": "Because it was two-tired!"
+    },
+    {
+      "setup": "How do you organize a space party?",
+      "punchline": "You 'planet'!"
+    },
+    {
+      "setup": "Why did the math book look sad?",
+      "punchline": "Because it had too many problems."
+    },
+    {
+      "setup": "What's brown and sticky?",
+      "punchline": "A stick!"
+    },
+    {
+      "setup": "How do you catch a squirrel?",
+      "punchline": "Climb up in a tree and act like a nut!"
+    },
+    {
+      "setup": "Why don't oysters donate to charity?",
+      "punchline": "Because they are shellfish!"
+    },
+    {
+      "setup": "I used to play piano by ear, but now I use my hands.",
+      "punchline": "It's more effective that way!"
+    },
+    {
+      "setup": "What's orange and sounds like a parrot?",
+      "punchline": "A carrot!"
+    },
+    {
+      "setup": "Why can't you hear a pterodactyl using the bathroom?",
+      "punchline": "Because the 'P' is silent!"
+    },
+    {
+      "setup": "What do you call a can opener that doesn't work?",
+      "punchline": "A can't opener!"
+    },
+    {
+      "setup": "How do you organize a space party?",
+      "punchline": "You 'planet'!"
+    },
+    {
+      "setup": "Did you hear about the claustrophobic astronaut?",
+      "punchline": "He just needed a little space!"
+    },
+    {
+      "setup": "Why don't scientists trust stairs?",
+      "punchline": "Because they're always up to something!"
+    },
+    {
+      "setup": "I told my wife she was drawing her eyebrows too high.",
+      "punchline": "She seemed surprised."
+    },
+    {
+      "setup": "Why don't skeletons fight each other?",
+      "punchline": "Because they don't have the guts!"
+    },
+    {
+      "setup": "Parallel lines have so much in common.",
+      "punchline": "It's a shame they'll never meet."
+    }
+  ]
+  
