4bb35ec38b80cd94d7aef78f86af397ffb295019
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2023-08-26 01:01:38 -0700

    using key props to dynamically inject values

--- AirBnb-clone/src/App.jsx
+++ AirBnb-clone/src/App.jsx
@@ -1,12 +1,34 @@
 import './App.css'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
+import Card from '../components/Card'
+import Data from './Data'
 
 function App() {
+  
+  const cardDetails = Data.map((datas) => {
+    return (
+      <Card
+          openSpots={datas.openSpots} 
+          key={datas.id}
+          img={datas.coverImg}
+          rating={datas.stats.rating}
+          reviewCount={datas.stats.reviewCount}
+          location={datas.location}
+          title={datas.title}
+          description={datas.description}
+          price={datas.price}
+  />
+    )
+  })
+
   return (
     <>
       <Navbar />
       <Hero />
+      <section className="cards-list">
+        {cardDetails}
+      </section>
     </>
   )
 }
