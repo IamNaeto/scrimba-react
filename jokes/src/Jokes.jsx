4f3486293ebed287eebe0f619a03c5b0bbb3c82b
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2023-09-02 11:28:11 -0700

    conditional rendering &&

--- jokes/src/Jokes.jsx
+++ jokes/src/Jokes.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import './App.css'
+import { useState } from "react";
 
 const Jokes = (props) => {
+
+    const [isShown, setIsShown] = useState(false)
+
+    function toggleShown(){
+        setIsShown(prevIsShown =>{
+            return(
+                prevIsShown = !prevIsShown
+            )
+        })
+    }
     return ( 
         <div className="jokes-box">
             {props.Setup && <h3 className="setup">{props.Setup}</h3>}
-            <p className="punchline">{props.Punchline}</p>
+            {isShown && <p className="punchline">{props.Punchline}</p>}
+            <button onClick={toggleShown}>Show Punchline</button>
         </div>
      );
 }
