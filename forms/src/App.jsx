a7d2b315161ed8f798379b909bec7686fbc0cf5c
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2023-09-08 15:58:16 -0700

    checkboxes and state

--- forms/src/App.jsx
+++ forms/src/App.jsx
@@ -3,58 +3,65 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    comments: ""
-  })
-
-  console.log(formData)
-
-  function handleChange(event){
-    setFormData(prevFormData =>{
-      return{
-        ...prevFormData,
-        [event.target.name]: event.target.value
-      }
-    })
-  }
-
-  return (
-    <form>
-      <input 
-      type="text"
-      onChange={handleChange}
-      placeholder='First Name'
-      name='firstName'
-      value={formData.firstName}
-      />
-
-      <input 
-      type="text"
-      onChange={handleChange}
-      placeholder='Last Name'
-      name='lastName'
-      value={formData.last}
-      />
-
-      <input 
-      type="text"
-      onChange={handleChange}
-      placeholder='Email'
-      name='email'
-      value={formData.email}
-      />
-
-      <textarea 
-      value={formData.comments}
-      placeholder="Comments"
-      onChange={handleChange}
-      name="comments"
-      />
-    </form>
-  )
+ const [formData, setFormData] = React.useState(
+        {
+            firstName: "", 
+            lastName: "", 
+            email: "", 
+            comments: "", 
+            isFriendly: true
+        }
+    )
+    
+    function handleChange(event) {
+        const {name, value, type, checked} = event.target
+        setFormData(prevFormData => {
+            return {
+                ...prevFormData,
+                [name]: type === "checkbox" ? checked : value
+            }
+        })
+    }
+    
+    return (
+        <form>
+            <input
+                type="text"
+                placeholder="First Name"
+                onChange={handleChange}
+                name="firstName"
+                value={formData.firstName}
+            />
+            <input
+                type="text"
+                placeholder="Last Name"
+                onChange={handleChange}
+                name="lastName"
+                value={formData.lastName}
+            />
+            <input
+                type="email"
+                placeholder="Email"
+                onChange={handleChange}
+                name="email"
+                value={formData.email}
+            />
+            <textarea 
+                value={formData.comments}
+                placeholder="Comments"
+                onChange={handleChange}
+                name="comments"
+            />
+            <input 
+                type="checkbox" 
+                id="isFriendly" 
+                checked={formData.isFriendly}
+                onChange={handleChange}
+                name="isFriendly"
+            /><label htmlFor="isFriendly">Are you friendly?</label>
+            <br />
+        </form>
+    )
 }
 
 export default App
