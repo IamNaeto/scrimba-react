7064199475f9d78056ad1c80cc2235aa110902f5
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2023-09-11 07:26:23 -0700

    window tracker using state and effect

---
+++ window-tracker/src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css'
+import React from 'react'
+import { useState } from 'react'
+import WindowTracker from './WindowTracker.JSX'
+
+function App() {
+   /**
+     * Challenge:
+     * 1. Create state called `show`, default to `true`
+     * 2. When the button is clicked, toggle `show`
+     * 3. Only display `<WindowTracker>` if `show` is `true`
+     */
+    
+   const [show, setShow] =useState(true)
+    
+   function toggle() {
+       setShow(prevShow => !prevShow)
+   }
+   
+   return (
+       <div className="container">
+           <button onClick={toggle}>
+               Toggle WindowTracker
+           </button>
+           {show && <WindowTracker />}
+       </div>
+   )
+}
+
+export default App
